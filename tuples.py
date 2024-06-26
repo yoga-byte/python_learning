866de356e27cb397defe049f8fea8b123dd8b118
Yogesh <yogi.dwivedi30@gmail.com>
2024-06-26 16:41:07 +0530

    practice tuples

---
+++ tuples.py
@@ -0,0 +1,9 @@
+airports = [
+    ("O' Hare",'ORD'),
+    ("Los Angeles","LAX"),
+    ("Dailas/Fort Worth","DFW"),
+    ("Denver","DEN")
+]
+
+for air, code in airports:
+    print('The code for {} International Airport is {}'.format(air, code))
